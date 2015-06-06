5534b99642351b9fa149f68987210bb0d8034c86
andrefedalto <andrefedalto@gmail.com>
2015-06-06 20:28:05 +0100

    Movie Classes

---
+++ Assets/Scripts/Movie.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Movie {
+
+    private string name;
+    private string genre;
+    private double rating;
+    private int year;
+    private List<string> reviews;
+    private string trailer;
+
+    public Movie(string _name, string _genre, double _rating, int _year, string _trailer)
+    {
+        reviews = new List<string>();
+        this.name = _name;
+        this.genre = _genre;
+        this.rating = _rating;
+        this.year = _year;
+        this.trailer = _trailer;
+    }
+
+    public void addReview(string _review)
+    {
+        reviews.Add(_review);
+    }
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Movies.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Movies {
+
+    private List<Movie> movies;
+
+
+    public Movies()
+    {
+        Movie m;
+        this.movies = new List<Movie>();
+
+        m = new Movie("About Time", "Drama", 7.8, 2013, "abouttime.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+        m = new Movie("Back to the Future", "Adventure", 8.5, 1985, "backtothefuture.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+        m = new Movie("Despicable Me 2", "Animation", 7.5, 2013, "despicableme2.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+        m = new Movie("Ex Machina", "Drama", 7.8, 2015, "exmachina.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+        m = new Movie("Fast and Furious 6", "Action", 7.2, 2013, "fastandfurious6.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+        m = new Movie("Les Miserables", "Drama", 7.7, 2012, "lesmiserables.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+        m = new Movie("Oblivion", "Action", 7.0, 2013, "oblivion.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+        m = new Movie("Pitch Perfect", "Comedy", 7.3, 2012, "pitchperfect.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+        m = new Movie("Ted", "Comedy", 7.0, 2012, "ted.mp4");
+        m.addReview("This is a great movie");
+        movies.Add(m);
+
+
+
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
