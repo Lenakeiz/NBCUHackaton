0e75ce6d919327b9beb195d1929304dd167f6b9d
Lenakeiz <andrea.castegnaro@outlook.com>
2015-06-06 17:21:02 +0100

    Initial commit

---
+++ Assets/Scripts/FlyCam.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlyCam : MonoBehaviour
+{
+	public  float lookSensitivity  = 15.0f;   
+	public  float moveSensitivity  = 1.0f;   
+	public  float minPitch         = -60.0f; // The minimum pitch angle (looking down)
+	public  float maxPitch         = 60.0f;  // he maximum pitch angle (looking up).
+	public  float lookDamping        = 0.9f;   
+
+	private float   rotationY    = 0.0f;         
+	private Vector2 rotationVelocity = Vector2.zero; 
+
+	void Update()
+	{
+		if( Input.GetMouseButton(1) )
+		{
+			// Add to the look velocity.
+			rotationVelocity.x += Input.GetAxisRaw("Mouse X") * lookSensitivity * Time.deltaTime;
+			rotationVelocity.y += Input.GetAxisRaw("Mouse Y") * lookSensitivity * Time.deltaTime;
+		}
+		
+		// Calculate and set the values for the new rotation.
+		float rotationX = transform.localEulerAngles.y + rotationVelocity.x;
+		rotationY += rotationVelocity.y;
+		rotationY = Mathf.Clamp(rotationY, minPitch, maxPitch);
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0.0f);
+		
+		// Damp rotation velocity.
+		rotationVelocity *= lookDamping;
+
+	}
+}
---
+++ Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	public GUITexture gt;
+	public WWW wwwdata;
+	public string url = "http://igor.gold.ac.uk/~acast014/portfolio/bbSeason.ogg";
+	string url_sample = "http://www.unity3d.com/webplayers/Movie/sample.ogg";
+
+	// Use this for initialization
+	void Start () {
+		wwwdata =  new WWW(url);
+		gt = GetComponent<GUITexture>();
+
+		MovieTexture mm_text = wwwdata.movie as MovieTexture;
+
+		gt.texture = wwwdata.movie;
+
+		//this.transform.localScale = new Vector3(0.0f,0.0f,0.0f);
+		//this.transform.position = new Vector3(0.5f,0.5f,0.0f);
+
+		//gt.pixelInset = new Rect(-mm_text.width / 2.0f, mm_text.width / 2.0f, -mm_text.height / 2.0f, mm_text.height / 2.0f);
+
+	}
+
+	// Make sure we have gui texture and audio source
+	//@script RequireComponent (GUITexture)
+	//@script RequireComponent (AudioSource)
+	
+	// Update is called once per frame
+	void Update () {
+
+		MovieTexture m = gt.texture as MovieTexture;
+		if(!m.isPlaying && m.isReadyToPlay)
+		{
+			m.Play();
+		}
+	}
+}
